e67307dc835cfa41de3a2266f8844812a7a654f4
mscaldas <mcq1@cdc.gov>
2020-03-19 15:04:50 -0400

    replacing kton HttpClient for java.net because wasn't working inside docker container

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,23 @@
-FROM adoptopenjdk/openjdk11-openj9:jdk-11.0.1.13-alpine-slim
-COPY target/simple-rest-streamer*.jar simple-rest-streamer.jar
-EXPOSE 8080
-CMD java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -Dcom.sun.management.jmxremote -noverify ${JAVA_OPTS} -jar simple-rest-streamer.jar
+#FROM adoptopenjdk/openjdk11-openj9:jdk-11.0.1.13-alpine-slim
+#COPY target/simple-rest-streamer*.jar simple-rest-streamer.jar
+#EXPOSE 8080
+#CMD java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -Dcom.sun.management.jmxremote -noverify ${JAVA_OPTS} -jar simple-rest-streamer.jar
+
+
+# build stage
+FROM maven:3-jdk-11 as builder
+RUN mkdir -p /usr/src/app
+COPY . /usr/src/app
+WORKDIR /usr/src/app
+RUN mvn clean package -DskipTests=true
+
+
+# create Image stage
+FROM adoptopenjdk/openjdk11-openj9:x86_64-ubuntu-jdk-11.0.6_10_openj9-0.18.1-slim
+
+VOLUME /tmp
+
+COPY --from=builder  /usr/src/app/target/simple-rest-streamer-*.jar ./simple-rest-streamer.jar
+
+RUN sh -c 'touch ./simple-rest-streamer.jar'
+ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom -Dserver.port=${PORT}","-jar","./simple-rest-streamer.jar"]
--- src/main/kotlin/simple/rest/streamer/RestProxy.kt
+++ src/main/kotlin/simple/rest/streamer/RestProxy.kt
@@ -1,46 +1,53 @@
 package simple.rest.streamer
 
-import com.fasterxml.jackson.databind.JsonNode
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.apache.Apache
-import io.ktor.client.request.post
-import io.ktor.client.request.url
-import io.ktor.content.TextContent
-import io.ktor.http.ContentType
-import kotlinx.coroutines.runBlocking
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class RestProxy(@Inject val appConfig: AppConfig) {
 
-    inline fun callRestService(payload: String): String {
-        var newValue = payload
-//        val json = jacksonObjectMapper()
-        runBlocking {
-            val client = HttpClient(Apache).use { client ->
-                newValue = client.post {
-                    url(appConfig.serviceurl )
-//                     body = payload
-                    body = TextContent(payload, contentType = ContentType.Application.Json)
-                }
-                return@runBlocking newValue
-            }
-        }
-        return newValue
+
+    fun callRestService(payload: String): String {
+        val client = HttpClient.newBuilder().build()
+        val request = HttpRequest.newBuilder()
+                .uri(URI.create(appConfig.serviceurl))
+                .POST(HttpRequest.BodyPublishers.ofString(payload))
+                .build()
+        val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+        return response.body()
     }
 
-    fun callRestService(payload: JsonNode): JsonNode {
-        var newValue = payload
-        runBlocking {
-            val client = HttpClient(Apache).use { client ->
-                newValue = client.post {
-                    url(appConfig.serviceurl )
-                    body = payload
-                }
-                return@runBlocking newValue
-            }
-        }
-        return newValue
-    }
+//    fun callRestService(payload: String): String {
+//        var newValue = payload
+////        val json = jacksonObjectMapper()
+//        runBlocking {
+//            HttpClient(Apache).use { client ->
+//                newValue = client.post {
+//                    url(appConfig.serviceurl )
+////                     body = payload
+//                    body = TextContent(payload, contentType = ContentType.Application.Json)
+//                }
+//                return@runBlocking newValue
+//            }
+//        }
+//        return newValue
+//    }
+//
+//    fun callRestService(payload: JsonNode): JsonNode {
+//        var newValue = payload
+//        runBlocking {
+//            val client = HttpClient(Apache).use { client ->
+//                newValue = client.post {
+//                    url(appConfig.serviceurl )
+//                    body = payload
+//                }
+//                return@runBlocking newValue
+//            }
+//        }
+//        return newValue
+//    }
 }
